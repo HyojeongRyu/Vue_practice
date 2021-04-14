8808517e23d6c499cf11c4ea7f0eb51979116e79
HyojeongRyu <67587032+HyojeongRyu@users.noreply.github.com>
2021-04-14 23:51:34 +0900

    Update App.vue
    0414

--- code_scalper/src/App.vue
+++ code_scalper/src/App.vue
@@ -1,15 +1,30 @@
 <template>
+<div>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Hello Vue!</h1>
+  <h2>{{food}}</h2>
+  <input type="text" v-model="food"/>
+  <!-- v-html은 텍스트가 아닌 태그 그대로 받아들인다. -->
+  <div v-html = "alertMessage"></div>
+  <!-- 받을 데이터를 신뢰할 수 있을 때 사용 -->
+  <div>{{test}}</div>
+  <img v-bind:src="imageSource" alt="random">
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      animal: "monkey",
+      food: "banana",
+      choco: "milk",
+      alertMssage: "<h1>Caution!!!</h1>",
+      test: `<button onclick="document.querySelector('body').style.display = 'none'">test</button>`,
+      imageSource: "https://placeimg.com/100/100/any"
+    }
   }
 }
 </script>
@@ -22,5 +37,8 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+input{
+  font-size: 20px;
 }
 </style>
